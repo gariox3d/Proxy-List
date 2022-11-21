625eb5a262222c89a805457958bc5a9f607f2b24
Caliph Dev <admin@caliph.my.id>
2022-11-21 23:26:39 +0700

    Update update-data.sh

--- update-data.sh
+++ update-data.sh
@@ -1,10 +1,10 @@
 echo "Getting Proxy List..."
-d=`curl https://proxy.caliph.my.id/proxy/socks5`
+d=$(curl https://proxy.caliph.my.id/proxy/socks5)
 echo $d > socks5.txt
 echo "" > README.md
 echo "# Socks5 Proxy\n\n" >> README.md
-echo "## Proxy List\n" >> README.md
-echo "\`\`\`\n$($d)\n\`\`\`" >> README.md
+echo "## Proxy List" >> README.md
+echo "\`\`\`$(d)\`\`\`" >> README.md
 
 echo "Commit Result to Github"
 git config --global user.email "admin@caliph.my.id"
