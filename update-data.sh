9c77b78ec4439a3ff562dbea6559a55b5159ea8f
Caliph Dev <admin@caliph.my.id>
2022-11-21 23:20:32 +0700

    Create update-data.sh

---
+++ update-data.sh
@@ -0,0 +1,13 @@
+echo "Getting Proxy List..."
+d=`curl https://proxy.caliph.my.id/proxy/socks5`
+echo $d > socks5.txt
+echo "" > README.md
+echo "# Socks5 Proxy\n\n" >> README.md
+echo "## Proxy List\n" >> README.md
+echo "\`\`\`\n$($d)\n\`\`\`" >> README.md
+
+echo "Commit Result to Github"
+git config --global user.email "admin@caliph.my.id"
+git config --global user.name "caliphdev"
+git add -A
+git commit -m "Update Proxy List"
